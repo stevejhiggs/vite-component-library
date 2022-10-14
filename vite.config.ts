60dc28a7e351c5fc8d5e8075823c1189a9d18cac
Steve Higgs <steve@veed.io>
2022-10-14 12:08:37 +0100

    better externals

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,39 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import dts from 'vite-plugin-dts'
-import checker from 'vite-plugin-checker'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import dts from 'vite-plugin-dts';
+import checker from 'vite-plugin-checker';
+import { peerDependencies, dependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      jsxRuntime: 'classic', // will generate much smaller output in library mode
+    }),
     svgr(),
     dts({
       insertTypesEntry: true,
     }),
     !process.env.VITEST ? checker({ typescript: true }) : undefined,
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'MyLib',
-      // the proper extensions will be added
-      fileName: 'my-lib',
+      entry: resolve(__dirname, 'src', 'index.ts'),
+      fileName: (ext) => `index.${ext}.js`,
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'styled-components'],
+      external: [...Object.keys(peerDependencies), ...Object.keys(dependencies)],
     },
+    target: 'esnext',
+    sourcemap: true,
   },
-})
+});
